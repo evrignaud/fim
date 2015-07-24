7880057f07baeafd196524e2f084781c0fe5ebde
Etienne Vrignaud <evrignaud@gmail.com>
2015-07-24 20:54:35 +0200

    Add one test

--- src/test/java/org/fim/model/StateTest.java
+++ src/test/java/org/fim/model/StateTest.java
@@ -31,6 +31,8 @@ import org.junit.Test;
 
 public class StateTest extends StateAssert
 {
+	public static final String MODIFICATION_TIME = "2015/07/23 23:24:10";
+
 	private BuildableState a1;
 	private BuildableState a2;
 	private BuildableState b;
@@ -71,9 +73,33 @@ public class StateTest extends StateAssert
 	@Test
 	public void weCanHashAState() throws ParseException
 	{
+		fixTimeStamps(a1);
+
+		String a1_hash = a1.hashState();
+		assertThat(a1_hash).isEqualTo("fe5c535a2fba07f111af4f4c27713d41c30df2cba5d9748740291bb711483518142e6e56fc8769256782fa0b09d6d4a83d572094e687e990476cb36df18a33d6");
+	}
+
+	@Test
+	public void hashDependsOnContent() throws ParseException
+	{
+		fixTimeStamps(a1);
+		fixTimeStamps(a2);
+		fixTimeStamps(b);
+
+		String a1_hash = a1.hashState();
+		String a2_hash = a2.hashState();
+		String b_hash = b.hashState();
+		
+		assertThat(a1_hash).isEqualTo(a2_hash);
+
+		assertThat(a1_hash).isNotEqualTo(b_hash);
+	}
+
+	private void fixTimeStamps(BuildableState a1) throws ParseException
+	{
 		// Fix the timeStamps in order that state hash can be verified
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
-		Date date = sdf.parse("2015/07/23 23:24:10");
+		Date date = sdf.parse(MODIFICATION_TIME);
 		long timestamp = date.getTime();
 
 		a1.setTimestamp(timestamp);
@@ -81,8 +107,5 @@ public class StateTest extends StateAssert
 		{
 			fileState.setLastModified(timestamp);
 		}
-
-		String hash = a1.hashState();
-		assertThat(hash).isEqualTo("fe5c535a2fba07f111af4f4c27713d41c30df2cba5d9748740291bb711483518142e6e56fc8769256782fa0b09d6d4a83d572094e687e990476cb36df18a33d6");
 	}
 }
