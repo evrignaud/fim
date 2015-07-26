ecf5a2d92965d070f3b5df657fcb6d462210d53f
Etienne Vrignaud <evrignaud@gmail.com>
2015-07-26 14:35:17 +0200

    Wait for the Queue to be filled by the main StateGenerator thread

--- src/main/java/org/fim/internal/FileHasher.java
+++ src/main/java/org/fim/internal/FileHasher.java
@@ -89,6 +89,8 @@ class FileHasher implements Runnable
 	{
 		try
 		{
+			waitQueueToBeFilled(2 * 1000);
+
 			Path file;
 			while ((file = filesToHash.poll(500, TimeUnit.MILLISECONDS)) != null)
 			{
@@ -114,6 +116,33 @@ class FileHasher implements Runnable
 		{
 			Logger.error(ex);
 		}
+	}
+
+	private void waitQueueToBeFilled(long timeout)
+	{
+		long sleepTime = 200;
+		long maxCount = timeout / sleepTime;
+		for (int index = 0; index < maxCount; index++)
+		{
+			if (!isEmpty())
+			{
+				break;
+			}
+
+			try
+			{
+				Thread.sleep(sleepTime);
+			}
+			catch (InterruptedException e)
+			{
+				break;
+			}
+		}
+	}
+
+	private boolean isEmpty()
+	{
+		return filesToHash.size() == 0;
 	}
 
 	private String getNormalizedFileName(Path file)
