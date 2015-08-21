08e46d14465da8ec62bdc4b323a556dc05643e36
Etienne Vrignaud <evrignaud@gmail.com>
2015-08-21 23:49:12 +0200

    Add eof

--- fim.bat
+++ fim.bat
@@ -35,3 +35,5 @@ for %%f in ("%baseDir%target\fim-*.jar") do (
 set JAR_FILE=%JAR_FILE:-sources=%
 
 java %JAVA_OPTIONS% -jar "%baseDir%target\%JAR_FILE%" "%1" "%2" "%3" "%4" "%5" "%6"
+
+@rem -------------------------------------------------------------------------------------------------------------------
