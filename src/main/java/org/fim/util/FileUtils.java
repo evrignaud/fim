0c55339803de29522e09491df413cf21baa08fa6
Etienne Vrignaud <evrignaud@gmail.com>
2015-08-21 23:45:16 +0200

    Fix file header

--- src/main/java/org/fim/util/FileUtils.java
+++ src/main/java/org/fim/util/FileUtils.java
@@ -1,11 +1,26 @@
+/*
+ * This file is part of Fim - File Integrity Manager
+ *
+ * Copyright (C) 2015  Etienne Vrignaud
+ *
+ * Fim is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Fim is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Fim.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.fim.util;
 
 import java.io.File;
 import java.nio.file.Path;
 
-/**
- * Created by evrignaud on 19/08/15.
- */
 public class FileUtils
 {
 	public static String getNormalizedFileName(Path file)
