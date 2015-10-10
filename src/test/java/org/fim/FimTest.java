5c0937712c456403f20f983689a82ba7ac3f597f
Etienne Vrignaud <evrignaud@gmail.com>
2015-10-10 10:58:03 +0200

    Add FimTest

--- src/test/java/org/fim/FimTest.java
+++ src/test/java/org/fim/FimTest.java
@@ -16,46 +16,23 @@
  * You should have received a copy of the GNU General Public License
  * along with Fim.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.fim.command;
+package org.fim;
 
-import org.fim.model.Context;
+import org.junit.Test;
 
-public class VersionCommand extends AbstractCommand
+public class FimTest
 {
-	@Override
-	public String getCmdName()
+	Fim cut = new Fim();
+
+	@Test
+	public void weCanPrintUsage()
 	{
-		return "version";
+		Fim.printUsage();
 	}
 
-	@Override
-	public String getShortCmdName()
+	@Test
+	public void weCanRunGetVersion() throws Exception
 	{
-		return "";
-	}
-
-	@Override
-	public String getDescription()
-	{
-		return "Prints the Fim version";
-	}
-
-	@Override
-	public FimReposConstraint getFimReposConstraint()
-	{
-		return FimReposConstraint.DONT_CARE;
-	}
-
-	@Override
-	public Object execute(Context context) throws Exception
-	{
-		String implementationVersion = this.getClass().getPackage().getImplementationVersion();
-		if (implementationVersion == null)
-		{
-			implementationVersion = "dev";
-		}
-
-		System.out.println("Fim version " + implementationVersion);
-		return null;
+		cut.main(new String[]{"-v"});
 	}
 }
